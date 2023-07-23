e70224cfa72074ccf177657f1c5fd82099a38dbd
VarinCode <94356640+VarinCode@users.noreply.github.com>
2023-07-23 09:31:50 +0700

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+  import Moon from "./components/Moon.svelte";  
+  import Article from "./components/Article.svelte";
+</script>
+
+<section class="container">
+  <Moon/>
+  <Article/>
+</section>
+
+<style>
+  .container{
+      display: grid;
+      place-items: center;
+      margin: 120px auto;
+      width: 100%;
+      height: max-content;
+  }
+</style>
---
+++ src/components/Moon.svelte
@@ -0,0 +1,23 @@
+<script>
+    const url = "https://pngimg.com/uploads/moon/moon_PNG10.png";
+</script>
+
+<img src={url} alt="moon" id="the-moon">
+
+<style>
+    :root{
+        --light-moon:#fdfcfc;
+        --shadow:#1d1d1d;
+    }
+    #the-moon{
+        width: 280px;
+        height: 280px;
+        transition: all .7s ease-in-out;
+        cursor: default;
+        filter: drop-shadow(9px 14px 11px var(--shadow));
+    }
+    #the-moon:hover{
+        scale: 125%;
+        filter: drop-shadow(0 0 23px var(--light-moon));
+    }
+</style>
